281e2f3bcc5256c13326fb32cf6ac7068d0b5299
Michał <michal.rymarski2003@gmail.com>
2023-12-28 14:30:01 +0100

    logowanie i zapis profilu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,7 +38,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     packaging {
         resources {
@@ -48,6 +49,12 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-auth")
+
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -56,6 +63,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/projekt_koncowy/MainActivity.kt
+++ app/src/main/java/com/example/projekt_koncowy/MainActivity.kt
@@ -1,43 +1,97 @@
 package com.example.projekt_koncowy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.projekt_koncowy.ui.theme.Projekt_KoncowyTheme
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.concurrent.thread
+import kotlin.math.log
+
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.d("chuj" , "Zalogowany")
+        } else {
+            Log.d("chuj" , "Nie zalogowany")
+        }
+
+
+
         setContent {
-            Projekt_KoncowyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize() , color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Surface(modifier = Modifier.fillMaxSize() , color = MaterialTheme.colorScheme.background) {
+                Row {
+                    Button(onClick = {
+                        auth.createUserWithEmailAndPassword("mihau123123@gmail.com" , "qwqwqw123").addOnSuccessListener {
+                            Log.d("chuj" , "Udalo sie stworzyc")
+                            Firebase.firestore.collection("profile")
+                                .add(
+                                    hashMapOf(
+                                        "nick" to "Mihau" ,
+                                        "email" to "mihau123123@gmail.com" ,
+                                        "password" to "qwqwqw123"
+                                    )
+                                ).addOnSuccessListener {
+                                    Log.d("chuj" , "Udalo sie stworzyc")
+                                }.addOnFailureListener {
+                                    Log.d("chuj" , "Nie udalo sie stworzyc")
+                                }
+
+                        }.addOnFailureListener {
+                            Log.d("chuj" , "Nie udalo sie stworzyc")
+                        }
+                    }) {
+                        Text(text = "stworz uzytkownika")
+                    }
+                    Button(onClick = {
+                        auth.signInWithEmailAndPassword("mihau123123@gmail.com" , "qwqwqw123").addOnSuccessListener {
+                            Log.d("chuj" , "Udalo sie zalogowac")
+                            Firebase.firestore.collection("profile")
+                                .whereEqualTo("email" , "mihau123123@gmail.com")
+                                .get()
+                                .addOnSuccessListener { documents ->
+                                    for (document in documents) {
+                                        Log.d("chuj" , "${document.id} => ${document.data}")
+                                    }
+                                }
+                        }.addOnFailureListener {
+                            Log.d("chuj" , "Nie udalo sie zalogowac")
+                        }
+                    }) {
+                        Text(text = "zaloguj uzytkownika")
+                    }
+
                 }
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String , modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!" ,
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Projekt_KoncowyTheme {
-        Greeting("Android")
-    }
-}
